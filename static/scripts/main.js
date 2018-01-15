e34748cd06be9752c844342c9984c723199a3b6f
Allen <502575260@qq.com>
2018-01-15 17:41:21 +0800

    auxiliary.html  require.js

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -8,6 +8,8 @@ requirejs.config({
         ],
         /** require CSS文件 */
         'css': 'css.min',
+        /** 缓动js文件 */
+        'ease': 'https://cdn.bootcss.com/jquery-easing/1.4.1/jquery.easing.min',
         /** SuperScrollorama.min.js CDN */
         'SuperScrollorama': 'https://cdn.bootcss.com/SuperScrollorama/1.0.3/jquery.superscrollorama.min',
         /** TweenMax.min.js CDN */
@@ -23,15 +25,17 @@ requirejs.config({
         /** wow CDN */
         'wow': 'https://cdn.bootcss.com/wow/1.1.2/wow.min',
         /** aos CDN */
-        'aos': 'https://cdn.bootcss.com/aos/2.2.0/aos.js'
+        'aos': 'https://cdn.bootcss.com/aos/2.2.0/aos'
     },
     shim: {
         /** 引入swiper依赖css文件 */
         'Swiper': ['css!https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/css/swiper.min.css'],
         'SuperScrollorama': ['jquery'],
+        /** 引入缓动js文件依赖 */
+        'ease': ['jquery'],
         /** 引入wow依赖css文件 */
         'wow': ['css!../styles/lib/animate.css'],
         /** 引入aos依赖css文件 */
-        'aos': ['css!https://cdn.bootcss.com/aos/2.2.0/aos.css']
+        'aos': ['jquery', 'ease','css!https://cdn.bootcss.com/aos/2.2.0/aos.css']
     }
 })
