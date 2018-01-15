7e2d3af700d5ee9e627fdaf0c1f9ccc1bc4c9378
Allen <502575260@qq.com>
2018-01-15 17:00:23 +0800

    删除多余的js文件； 模块化js模板完成。

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -6,14 +6,32 @@ requirejs.config({
             'https://cdn.bootcss.com/jquery/1.8.3/jquery.min',
             'lib/jquery-1.8.3.min'
         ],
+        /** require CSS文件 */
+        'css': 'css.min',
+        /** SuperScrollorama.min.js CDN */
         'SuperScrollorama': 'https://cdn.bootcss.com/SuperScrollorama/1.0.3/jquery.superscrollorama.min',
+        /** TweenMax.min.js CDN */
         'tweenMax': 'https://cdn.bootcss.com/gsap/latest/TweenMax.min',
+        /** Swiper CDN */
         'Swiper': 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/js/swiper.min',
+        /** echarts路径 */
         'echarts': 'lib/echarts.min',
+        /** 中国地图js文件 */
         'china': 'lib/china',
-        'parallax': 'parallax'
+        /** 视差效果js文件 */
+        'parallax': 'parallax',
+        /** wow CDN */
+        'wow': 'https://cdn.bootcss.com/wow/1.1.2/wow.min',
+        /** aos CDN */
+        'aos': 'https://cdn.bootcss.com/aos/2.2.0/aos.js'
     },
     shim: {
-        'SuperScrollorama': ['jquery']
+        /** 引入swiper依赖css文件 */
+        'Swiper': ['css!https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/css/swiper.min.css'],
+        'SuperScrollorama': ['jquery'],
+        /** 引入wow依赖css文件 */
+        'wow': ['css!../styles/lib/animate.css'],
+        /** 引入aos依赖css文件 */
+        'aos': ['css!https://cdn.bootcss.com/aos/2.2.0/aos.css']
     }
 })
